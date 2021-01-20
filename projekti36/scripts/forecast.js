5a0251d19b09181f8ceff555960bec549d32591e
GengarWasTaken <72507176+GengarWasTaken@users.noreply.github.com>
2021-01-20 16:29:25 +0100

    Update forecast.js

--- projekti36/scripts/forecast.js
+++ projekti36/scripts/forecast.js
@@ -1,8 +1,61 @@
-const key = "IqlGl7nvnMkHRemMREeGwWs7jj7L3WkK";
+class Forecast {
 
+    constructor() {
+
+        this.key = "*********************************";
+        this.weatherURI = "http://dataservice.accuweather.com/currentconditions/v1/";
+        this.cityURI = "http://dataservice.accuweather.com/locations/v1/cities/search";
+
+    }
+
+    async updateCity(city) {
+
+        const cityDets = await this.getCity(city);
+
+        const weather = await this.getWeather(cityDets.Key);
+    
+        return {
+           cityDets,
+           weather
+        };
+
+    }
+
+    async getCity(city) {
+
+        const query = `?apikey=${this.key}&q=${city}`;
+
+        const response = await fetch(this.cityURI + query);
+
+        const data = await response.json();
+
+        return data[0];
+
+    }
+
+    async getWeather(id) {
+
+        const query = `${id}?apikey=${this.key}`;
+    
+        const response = await fetch(this.weatherURI + query);
+        
+        const data = await response.json();
+    
+        return data[0];
+
+    }
+
+}
+
+
+
+
+//OLD WAY OF DOING THINGS 
+
+/* const key = "*********************************"; */
 
 // get weather information
-const getWeather = async (id) => {
+/* const getWeather = async (id) => {
 
     const base = "http://dataservice.accuweather.com/currentconditions/v1/";
     const query = `${id}?apikey=${key}`;
@@ -25,14 +78,4 @@ const data = await response.json();
 
 return data[0];
 
-};
-
-
-/* getCity("london")
-.then(data => {
-    return getWeather(data.Key);
-}).then( data => {
-    console.log(data);
-}).catch(err => {
-    console.log(err);
-}); */
+}; */
